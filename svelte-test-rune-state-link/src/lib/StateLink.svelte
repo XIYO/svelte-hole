97737a805d7fabb7c885dd5a22f798240ca23363
XIYO <master@xiyo.dev>
2024-08-21 18:57:36 +0900

    :rocket: 테스트 완료

---
+++ svelte-test-rune-state-link/src/lib/StateLink.svelte
@@ -0,0 +1,8 @@
+<script>
+	let { value = $bindable() } = $props();
+
+	let stateLinkValue = $state.link(value);
+</script>
+
+<h2>bindable - props로 전달받은 값을 state link 로 래핑</h2>
+<button onclick={() => {stateLinkValue++}}>{stateLinkValue}</button>
---
+++ svelte-test-rune-state-link/tests/test.js
@@ -0,0 +1,17 @@
+import { expect, test } from '@playwright/test';
+
+test('home page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.locator('h1')).toBeVisible();
+});
+
+test('show h2', async ({ page }) => {
+	await page.goto('/');
+
+	// 버튼을 찾고 클릭
+	const button = page.locator('button#show-h2'); // 버튼에 id="show-h2" 속성이 있다고 가정
+	await button.click();
+
+	// h2 요소가 화면에 나타나는지 확인
+	await expect(page.locator('h2')).toBeVisible();
+});
